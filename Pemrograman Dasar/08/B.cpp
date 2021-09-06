b4ea9ab9041c7599c29ae3bc25449c82c8ef7b83
Hokky Aryasta <81088460+hokkyar@users.noreply.github.com>
2021-09-06 15:07:16 +0800

    adding syntax

--- Pemrograman Dasar/08/B.cpp
+++ Pemrograman Dasar/08/B.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <bits/stdc++.h>
+#include <cmath>
 
 using namespace std;
 
@@ -11,8 +12,25 @@ int main(){
 	
 	for(int i = 1; i <= n; i++){
 		cin >> bil;
+		bool prima = true;
+		if(bil < 2){
+			prima = false;
+		}
+		int div = 2;
+		while(div < trunc(sqrt(bil))){
+			if(bil % div == 0){
+				prima = false;
+				break;
+			}
+			div++;
+		}
+		
+		if(prima){
+			cout << "YA" << endl;
+		}else{
+			cout << "BUKAN" << endl;
+		}
 	}
 
 	return 0;
 }
-
